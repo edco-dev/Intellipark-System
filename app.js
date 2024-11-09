a5a027c7ebcf9b656fd4057bf9ce4160cc0e34be
Gerico Mangali <gmangali@seekasia.com>
2024-11-09 21:53:37 +0800

    environment variables

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDbXiBef-fXk9igZ5mZv_F896kyPtnXxvk",
+//     authDomain: "intellipark-db283.firebaseapp.com",
+//     projectId: "intellipark-db283",
+//     storageBucket: "intellipark-db283.appspot.com",
+//     messagingSenderId: "159684284966",
+//     appId: "1:159684284966:web:5149d6318931c0bedc4ce7"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const auth = getAuth(app);
+
+// export { db, auth };
+
+//Production
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDbXiBef-fXk9igZ5mZv_F896kyPtnXxvk",
-    authDomain: "intellipark-db283.firebaseapp.com",
-    projectId: "intellipark-db283",
-    storageBucket: "intellipark-db283.appspot.com",
-    messagingSenderId: "159684284966",
-    appId: "1:159684284966:web:5149d6318931c0bedc4ce7"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
