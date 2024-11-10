b5a0552764505c15a2674bb35bffceaf96f78d8b
Gerico Mangali <gmangali@seekasia.com>
2024-11-10 11:23:26 +0800

    fix api error 1

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDbXiBef-fXk9igZ5mZv_F896kyPtnXxvk",
-//     authDomain: "intellipark-db283.firebaseapp.com",
-//     projectId: "intellipark-db283",
-//     storageBucket: "intellipark-db283.appspot.com",
-//     messagingSenderId: "159684284966",
-//     appId: "1:159684284966:web:5149d6318931c0bedc4ce7"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// const auth = getAuth(app);
-
-// export { db, auth };
-
-//Production
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.PARCEL_APP_FIREBASE_API_KEY,
     authDomain: process.env.PARCEL_APP_FIREBASE_AUTH_DOMAIN,
@@ -32,8 +12,13 @@ const firebaseConfig = {
     appId: process.env.PARCEL_APP_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
+    
+console.log(process.env.PARCEL_APP_FIREBASE_API_KEY);
