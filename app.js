8a02d04faaa5a33f6ad0178e19159d6a5e63258e
Gerico Mangali <gmangali@seekasia.com>
2024-11-10 11:17:22 +0800

    fix error dotentv

--- app.js
+++ app.js
@@ -18,19 +18,18 @@
 // export { db, auth };
 
 //Production
-import 'dotenv/config'; // Ensure this line is at the top
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.APP_FIREBASE_API_KEY,
-    authDomain: process.env.APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.APP_FIREBASE_APP_ID,
+    apiKey: process.env.PARCEL_APP_FIREBASE_API_KEY,
+    authDomain: process.env.PARCEL_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.PARCEL_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.PARCEL_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.PARCEL_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.PARCEL_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
